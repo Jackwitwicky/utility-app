273fb1f72440f8bc74ad93a7be3943fea2694e6b
Jack Witwicky <Jack Kiarie>
2020-05-06 11:33:58 +0300

    initial commit

---
+++ app/src/main/java/com/gizahackathon/utilitiesapp/ui/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.gizahackathon.utilitiesapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.gizahackathon.utilitiesapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        //splash screen timer
+        const val SPLASH_TIME_OUT : Long = 3000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            val homeIntent = Intent(this@SplashActivity, HomeActivity::class.java)
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
