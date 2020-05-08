6fdad57e9d750d11fb68a9f216483a13b7bd7d83
Jack Witwicky <Jack Kiarie>
2020-05-08 16:26:24 +0300

    added home and new bill design

---
+++ app/src/main/java/com/gizahackathon/utilitiesapp/ui/addbill/AddBillActivity.kt
@@ -0,0 +1,13 @@
+package com.gizahackathon.utilitiesapp.ui.addbill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gizahackathon.utilitiesapp.R
+
+class AddBillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_bill)
+    }
+}
